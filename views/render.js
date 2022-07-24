6de87da099cfc4989557d54280137f6865a714fe
Emmanuel Adesile <emmanueladesile@Emmanuels-MacBook-Pro-2.local>
2022-07-24 20:29:44 +0100

    reach-tut

---
+++ views/render.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom";
+
+export function renderDOM(app) {
+  // const container = document.getElementById("app");
+  // const root = createRoot(container);
+  ReactDOM.render(
+    <React.StrictMode>{app}</React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+export function renderView(parent, Views) {
+  parent.state = parent.state || {};
+  const { view, ContentView } = parent.state;
+  const View = view === "Wrapper" ? ContentView : Views[view];
+  const Wrapper = Views["Wrapper"];
+  const props = { ...parent.props, ...parent.state, parent };
+  const content = <View {...props} />;
+  return <Wrapper {...{ content }} />;
+}
